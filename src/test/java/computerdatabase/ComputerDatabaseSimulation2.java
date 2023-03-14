057ebe7ebcbf6eb827cfd2dd07881b991ffcd2b5
tcadoret <tcadoret@gatling.io>
2023-03-14 10:54:49 +0100

    longer sim

---
+++ src/test/java/computerdatabase/ComputerDatabaseSimulation2.java
@@ -0,0 +1,39 @@
+package computerdatabase;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+/**
+ * This sample is based on our official tutorials:
+ * <ul>
+ *   <li><a href="https://gatling.io/docs/gatling/tutorials/quickstart">Gatling quickstart tutorial</a>
+ *   <li><a href="https://gatling.io/docs/gatling/tutorials/advanced">Gatling advanced tutorial</a>
+ * </ul>
+ */
+public class ComputerDatabaseSimulation2 extends Simulation {
+
+    ChainBuilder search =
+        exec(http("Home "+ System.getenv("vireya")).get("/"));
+
+    HttpProtocolBuilder httpProtocol =
+        http.baseUrl("https://computer-database.gatling.io")
+            .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .acceptLanguageHeader("en-US,en;q=0.5")
+            .acceptEncodingHeader("gzip, deflate")
+            .userAgentHeader(
+                "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0"
+            );
+
+    ScenarioBuilder users = scenario("Users").exec(search);
+
+    {
+        setUp(
+            users.injectOpen(stressPeakUsers(200).during(30))
+        ).protocols(httpProtocol);
+    }
+}
